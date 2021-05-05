e7ca9bed9e64201df78b1df4c1b339ef25089275
andrey <suha1253516@gmail.com>
2021-05-05 19:46:21 +0300

    dvae quantize option without gradient and gumbel added

--- modules/dvae/funcs.py
+++ modules/dvae/funcs.py
@@ -1,9 +1,9 @@
 import torch
 
 
-def ng_quantize(z_logits):
+def ng_quantize(z_logits, dim=1):
     z = torch.zeros(z_logits.size(), device=z_logits.device)
-    index = z_logits.argmax(dim=1)
-    z = torch.scatter(z, 1, index.unsqueeze(dim=1), 1.0)
+    index = z_logits.argmax(dim=dim)
+    z = torch.scatter(z, dim, index.unsqueeze(dim=dim), 1.0)
     return z
 
