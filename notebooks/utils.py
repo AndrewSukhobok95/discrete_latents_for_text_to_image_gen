c6fe33fcd33120d7810c4a170ef4c2cfa41a0e97
andrey <suha1253516@gmail.com>
2021-05-18 19:46:19 +0300

    show function modification for the notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,12 +1,19 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
 
 
-def show(img, figsize=(8, 4)):
-    img_grid = make_grid(img.detach().cpu())
+def show(img, figsize=(8, 4), plot_grid=False):
+    img_cpu = img.detach().cpu()
+    if plot_grid:
+        b, ch, h, w = img_cpu.size()
+        bg = torch.ones(b, ch, h+2, w+2) * 0.7
+        bg[:, :, 1:h+1, 1:w+1] = img_cpu
+        npimg = make_grid(bg).numpy()
+    else:
+        npimg = make_grid(img_cpu).numpy()
     plt.figure(figsize=figsize)
-    npimg = img_grid.numpy()
     fig = plt.imshow(np.transpose(npimg, (1, 2, 0)), interpolation='nearest')
     fig.axes.get_xaxis().set_visible(False)
     fig.axes.get_yaxis().set_visible(False)
