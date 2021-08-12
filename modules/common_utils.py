dcd243decfc364af62386be510da5ed85e3495f8
andrey <suha1253516@gmail.com>
2021-08-12 12:06:37 +0300

    img to latent func added

--- modules/common_utils.py
+++ modules/common_utils.py
@@ -7,10 +7,18 @@ def subsequent_mask(sz):
     return mask
 
 
-def latent_to_img(sample, dvae, hidden_height, heddin_width):
+def latent_to_img(sample, dvae, img_latent_height, img_latent_width):
     seq_len, batch, emb = sample.size()
-    latent = sample.permute(1, 2, 0).view(batch, -1, hidden_height, heddin_width)
+    latent = sample.permute(1, 2, 0).view(batch, -1, img_latent_height, img_latent_width)
     img = dvae.ng_q_decode(latent)
     return img
+
+
+def img_to_latent(sample, dvae):
+    with torch.no_grad():
+        latent = dvae.ng_q_encode(sample)
+    b, emb, h, w = latent.size()
+    x = latent.view(b, emb, -1).permute(2, 0, 1)
+    return x
 
 
